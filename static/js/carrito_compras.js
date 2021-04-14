9a8432e486e6e33896926575d910153f498c9728
hacker <hacker@hacker.home>
2021-04-13 23:27:02 -0500

    Listo el carrito de compras solo la cantidad va ser ingresar por pieza no por cantidad y hacer mensajes de alertas cada vez que se inserte un producot en el carrito

--- static/js/carrito_compras.js
+++ static/js/carrito_compras.js
@@ -1,8 +1,8 @@
 // Variables
 const carrito = document.querySelector('#carrito');
-const listaCursos = document.querySelector('#lista-productos');
+const listaProductos = document.querySelector('#lista-productos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 let articulosCarrito = [];
 
 // Listeners
@@ -10,34 +10,38 @@ cargarEventListeners();
 
 function cargarEventListeners() {
      // Dispara cuando se presiona "Agregar Carrito"
-     listaCursos.addEventListener('click', agregarCurso);
+     listaProductos.addEventListener('click', agregarCurso);
 
-     // Cuando se elimina un curso del carrito
+     // Cuando se elimina un producto del carrito
      carrito.addEventListener('click', eliminarCurso);
 
      // Al Vaciar el carrito
      vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 
+
+     // NUEVO: Contenido cargado
+     document.addEventListener('DOMContentLoaded', () => {
+          articulosCarrito = JSON.parse( localStorage.getItem('carrito') ) || []  ;
+          // console.log(articulosCarrito);
+          carritoHTML();
+     });
 }
 
 
-
-
-// Funciones
-// Función que añade  al carrito
+// Función que añade el producto al carrito
 function agregarCurso(e) {
      e.preventDefault();
      // Delegation para agregar-carrito
      if(e.target.classList.contains('agregar-carrito')) {
           const producto = e.target.parentElement.parentElement;
           // Enviamos el producto seleccionado para tomar sus datos
-          leerDatosCurso(producto);
+          leerDatosProducto(producto);
      }
 }
 
 // Lee los datos del producto
-function leerDatosCurso(producto) {
-     const infoproducto = {
+function leerDatosProducto(producto) {
+     const infoProducto = {
           imagen: producto.querySelector('img').src,
           titulo: producto.querySelector('h4').textContent,
           precio: producto.querySelector('.precio ').textContent,
@@ -46,35 +50,38 @@ function leerDatosCurso(producto) {
      }
 
 
-     if( articulosCarrito.some( producto => producto.id === infoproducto.id ) ) {
+     if( articulosCarrito.some( producto => producto.id === infoProducto.id ) ) {
           const productos = articulosCarrito.map( producto => {
-               if( producto.id === infoproducto.id ) {
-                    producto.cantidad++;
-                     return producto;
-                } else {
-                     return producto;
-             }
+               if( producto.id === infoProducto.id ) {
+                    let cantidad = parseInt(producto.cantidad);
+                    cantidad++
+                    producto.cantidad =  cantidad;
+                    return producto;
+               } else {
+                    return producto;
+               }
           })
           articulosCarrito = [...productos];
      }  else {
-          articulosCarrito = [...articulosCarrito, infoproducto];
+          articulosCarrito = [...articulosCarrito, infoProducto];
      }
 
      console.log(articulosCarrito)
 
-     
+
 
      // console.log(articulosCarrito)
      carritoHTML();
 }
 
-// Elimina el curso del carrito en el DOM
-function eliminarProducto(e) {
+// Elimina el producto del carrito en el DOM
+function eliminarCurso(e) {
      e.preventDefault();
-     if(e.target.classList.contains('borrar-curso') ) {
+     if(e.target.classList.contains('borrar-producto') ) {
           // e.target.parentElement.parentElement.remove();
-          const productoId = e.target.getAttribute('data-id')
-          
+          const producto = e.target.parentElement.parentElement;
+          const productoId = producto.querySelector('a').getAttribute('data-id');
+
           // Eliminar del arreglo del carrito
           articulosCarrito = articulosCarrito.filter(producto => producto.id !== productoId);
 
@@ -83,7 +90,7 @@ function eliminarProducto(e) {
 }
 
 
-// Muestra el curso seleccionado en el Carrito
+// Muestra el producto seleccionado en el Carrito
 function carritoHTML() {
 
      vaciarCarrito();
@@ -91,27 +98,32 @@ function carritoHTML() {
      articulosCarrito.forEach(producto => {
           const row = document.createElement('tr');
           row.innerHTML = `
-               <td>  
+               <td>
                     <img src="${producto.imagen}" width=100>
                </td>
                <td>${producto.titulo}</td>
                <td>${producto.precio}</td>
                <td>${producto.cantidad} </td>
                <td>
-                    <a href="#" class="borrar-curso" data-id="${producto.id}">X</a>
+                    <a href="#" class="borrar-producto" data-id="${producto.id}">X</a>
                </td>
           `;
           contenedorCarrito.appendChild(row);
      });
 
+     // NUEVO:
+     sincronizarStorage();
+
+}
+
+
+// NUEVO:
+function sincronizarStorage() {
+     localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
 }
 
 // Elimina los cursos del carrito en el DOM
 function vaciarCarrito() {
-     // forma lenta
-     // contenedorCarrito.innerHTML = '';
-
-
      // forma rapida (recomendada)
      while(contenedorCarrito.firstChild) {
           contenedorCarrito.removeChild(contenedorCarrito.firstChild);
